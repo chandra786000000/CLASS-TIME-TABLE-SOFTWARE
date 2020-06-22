ddc026b611ba7cdf5c713b5dca58f5e143daa2b5
chandra786000000 <mchandraprakash123@gmail.com>
2020-06-22 21:27:39 +0530

    first commit

---
+++ final1/Form1.Designer.vb
@@ -0,0 +1,267 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.bt5 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DGV2 = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.DGV1 = New System.Windows.Forms.DataGridView()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.bt6 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        CType(Me.DGV2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DGV1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Honeydew
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(3, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(797, 449)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Honeydew
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Panel3)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Controls.Add(Me.lbl1)
+        Me.Panel2.Controls.Add(Me.DGV1)
+        Me.Panel2.Controls.Add(Me.Panel4)
+        Me.Panel2.Location = New System.Drawing.Point(-3, -3)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(802, 454)
+        Me.Panel2.TabIndex = 2
+        '
+        'Panel3
+        '
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel3.Controls.Add(Me.bt5)
+        Me.Panel3.Controls.Add(Me.TextBox1)
+        Me.Panel3.Controls.Add(Me.Label2)
+        Me.Panel3.Controls.Add(Me.DGV2)
+        Me.Panel3.Location = New System.Drawing.Point(156, 215)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(633, 236)
+        Me.Panel3.TabIndex = 5
+        '
+        'bt5
+        '
+        Me.bt5.BackColor = System.Drawing.Color.LightGreen
+        Me.bt5.Location = New System.Drawing.Point(189, 40)
+        Me.bt5.Name = "bt5"
+        Me.bt5.Size = New System.Drawing.Size(75, 23)
+        Me.bt5.TabIndex = 7
+        Me.bt5.Text = "Search"
+        Me.bt5.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Honeydew
+        Me.TextBox1.Location = New System.Drawing.Point(22, 40)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(151, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.MintCream
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label2.Location = New System.Drawing.Point(22, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 28)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Generate" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Time-Table"
+        '
+        'DGV2
+        '
+        Me.DGV2.BackgroundColor = System.Drawing.Color.Honeydew
+        Me.DGV2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.DGV2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV2.Location = New System.Drawing.Point(22, 73)
+        Me.DGV2.Name = "DGV2"
+        Me.DGV2.Size = New System.Drawing.Size(609, 150)
+        Me.DGV2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.MintCream
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Location = New System.Drawing.Point(285, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 15)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "StudentData"
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.BackColor = System.Drawing.Color.MintCream
+        Me.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbl1.Location = New System.Drawing.Point(180, 14)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(88, 15)
+        Me.lbl1.TabIndex = 3
+        Me.lbl1.Text = "DeptCourseData"
+        '
+        'DGV1
+        '
+        Me.DGV1.BackgroundColor = System.Drawing.Color.Honeydew
+        Me.DGV1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.DGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV1.Location = New System.Drawing.Point(180, 32)
+        Me.DGV1.Name = "DGV1"
+        Me.DGV1.Size = New System.Drawing.Size(609, 150)
+        Me.DGV1.TabIndex = 1
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.MintCream
+        Me.Panel4.Controls.Add(Me.bt6)
+        Me.Panel4.Controls.Add(Me.Button4)
+        Me.Panel4.Controls.Add(Me.Button2)
+        Me.Panel4.Controls.Add(Me.Button3)
+        Me.Panel4.Controls.Add(Me.Button1)
+        Me.Panel4.Location = New System.Drawing.Point(3, 3)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(147, 448)
+        Me.Panel4.TabIndex = 0
+        '
+        'bt6
+        '
+        Me.bt6.BackColor = System.Drawing.Color.LightGreen
+        Me.bt6.Location = New System.Drawing.Point(3, 280)
+        Me.bt6.Name = "bt6"
+        Me.bt6.Size = New System.Drawing.Size(142, 59)
+        Me.bt6.TabIndex = 12
+        Me.bt6.Text = "Reset"
+        Me.bt6.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.LightGreen
+        Me.Button4.Location = New System.Drawing.Point(3, 212)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(142, 62)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "Load " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Student"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.LightGreen
+        Me.Button2.Location = New System.Drawing.Point(3, 144)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(142, 62)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Student Detail" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Enter Excel"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.LightGreen
+        Me.Button3.Location = New System.Drawing.Point(3, 76)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(142, 62)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Load " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Dept"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LightGreen
+        Me.Button1.Location = New System.Drawing.Point(3, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(142, 59)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Department " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Excel"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label3.Location = New System.Drawing.Point(180, 185)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(275, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Press Reset Button before giving new time table as input" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        CType(Me.DGV2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DGV1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel4.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents bt5 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DGV2 As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents DGV1 As DataGridView
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents bt6 As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ final1/Form1.vb
@@ -0,0 +1,189 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.OleDb
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim conn As OleDbConnection
+    Dim dta As OleDbDataAdapter
+    Dim dts As DataSet
+    Dim mysqlcon As SqlConnection
+    Dim command As SqlCommand
+    Dim fd As New OpenFileDialog
+    Dim strFileName As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            fd.Title = "Open File Dialog"
+            fd.InitialDirectory = "E:\"
+            fd.Filter = "All Files (*.*)|*.*|Excel Files(*.xlsx)|*.xlsx|Xls Files (*.xls)|*.xls "
+            fd.RestoreDirectory = True
+
+            If fd.ShowDialog(Me) = System.Windows.Forms.DialogResult.OK Then
+                Dim fi As New IO.FileInfo(fd.FileName)
+                Dim FileName As String = fd.FileName
+                strFileName = fi.FullName
+                conn = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + strFileName + ";Extended Properties=Excel 12.0;")
+                dta = New OleDbDataAdapter("select * from [Sheet1$]", conn)
+                dts = New DataSet
+                dta.Fill(dts, "[Sheet1$]")
+                DGV1.DataSource = dts
+                DGV1.DataMember = "[Sheet1$]"
+                conn.Close()
+                MessageBox.Show("You selected " & fd.FileName)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            MessageBox.Show("Time-Table is already imported")
+            conn.Close()
+            Exit Sub
+        End Try
+
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGV1.CellContentClick
+
+    End Sub
+    Dim connection As New MySqlConnection("datasource='localhost';username='com786000000';password='chandra@123';database='cs243'")
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            Dim cmd As MySqlCommand
+            connection.Open()
+            For i As Integer = 0 To DGV1.Rows.Count - 2 Step +1
+                cmd = New MySqlCommand("insert into department values(@Id,@D_ay,@Start_Time,@End_Time,@Course)", connection)
+                cmd.Parameters.Add("@Id", MySqlDbType.VarChar).Value = DGV1.Rows(i).Cells(0).Value.ToString()
+                cmd.Parameters.Add("@D_ay", MySqlDbType.VarChar).Value = DGV1.Rows(i).Cells(1).Value.ToString()
+                cmd.Parameters.Add("@Start_Time", MySqlDbType.VarChar).Value = DGV1.Rows(i).Cells(2).Value.ToString()
+                cmd.Parameters.Add("@End_Time", MySqlDbType.VarChar).Value = DGV1.Rows(i).Cells(3).Value.ToString()
+                cmd.Parameters.Add("@Course", MySqlDbType.VarChar).Value = DGV1.Rows(i).Cells(4).Value.ToString()
+                cmd.ExecuteNonQuery()
+            Next
+            connection.Close()
+            MsgBox("Data Successfully Imported !")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            connection.Close()
+            Exit Sub
+        End Try
+    End Sub
+    Dim connection1 As New MySqlConnection("datasource='localhost';username='com786000000';password='chandra@123';database='cs243'")
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            Dim cmd1 As MySqlCommand
+            connection1.Open()
+            For i As Integer = 0 To DGV1.Rows.Count - 2 Step +1
+                cmd1 = New MySqlCommand("insert into student_course_registered_table  values(@SCR_Id,@Stud_Id,@Course)", connection1)
+                cmd1.Parameters.Add("@SCR_Id", MySqlDbType.VarChar).Value = DGV1.Rows(i).Cells(0).Value.ToString()
+                cmd1.Parameters.Add("@Stud_Id", MySqlDbType.VarChar).Value = DGV1.Rows(i).Cells(1).Value.ToString()
+                cmd1.Parameters.Add("@Course", MySqlDbType.VarChar).Value = DGV1.Rows(i).Cells(2).Value.ToString()
+                cmd1.ExecuteNonQuery()
+            Next
+            connection1.Close()
+            MsgBox("Data Successfully Imported !")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            connection1.Close()
+            Exit Sub
+        End Try
+    End Sub
+    Dim conn1 As OleDbConnection
+    Dim dta1 As OleDbDataAdapter
+    Dim dts1 As DataSet
+    Dim mysqlcon1 As SqlConnection
+    Dim command1 As SqlCommand
+    Dim fd1 As New OpenFileDialog
+    Dim strFileName1 As String
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+
+            fd1.Title = "Open File Dialog"
+            fd1.InitialDirectory = "E:\"
+            fd1.Filter = "All Files (*.*)|*.*|Excel Files(*.xlsx)|*.xlsx|Xls Files (*.xls)|*.xls "
+            fd1.RestoreDirectory = True
+
+            If fd1.ShowDialog(Me) = System.Windows.Forms.DialogResult.OK Then
+                Dim fi1 As New IO.FileInfo(fd1.FileName)
+                Dim FileName1 As String = fd1.FileName
+                strFileName1 = fi1.FullName
+                conn1 = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + strFileName1 + ";Extended Properties=Excel 12.0;")
+                dta1 = New OleDbDataAdapter("select * from [Sheet1$]", conn1)
+                dts1 = New DataSet
+                dta1.Fill(dts1, "[Sheet1$]")
+                DGV1.DataSource = dts1
+                DGV1.DataMember = "[Sheet1$]"
+                conn1.Close()
+                MessageBox.Show("You selected " & fd1.FileName)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            MessageBox.Show("Time-Table is already imported")
+            conn.Close()
+            Exit Sub
+        End Try
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+    Dim command5 As MySqlCommand
+    Dim connection5 As New MySqlConnection("datasource='localhost';username='com786000000';password='chandra@123';database='cs243'")
+    Dim reader As MySqlDataReader
+    Private Sub bt5_Click(sender As Object, e As EventArgs) Handles bt5.Click
+        Try
+            Dim query As String
+            connection5.Open()
+            query = "select D_ay,Start_Time,End_Time,Course from department natural join Student_Course_Registered_Table where Stud_Id='" & TextBox1.Text & "' ORDER BY D_ay ASC"
+            command5 = New MySqlCommand(query, connection5)
+            reader = command5.ExecuteReader
+            Dim dt = New DataTable()
+            dt.Load(reader)
+
+            DGV2.AutoGenerateColumns = True
+            DGV2.DataSource = dt
+            DGV2.Refresh()
+            connection5.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            connection5.Close()
+        End Try
+
+    End Sub
+    Dim cmd6 As MySqlCommand
+    Dim conn6 As New MySqlConnection("datasource='localhost';username='com786000000';password='chandra@123';database='cs243'")
+
+    Private Sub bt6_Click(sender As Object, e As EventArgs) Handles bt6.Click
+        Try
+            Dim qry As String
+            qry = "delete FROM cs243.department"
+            Dim qry1 As String
+            qry1 = "delete FROM cs243.Student_Course_Registered_Table"
+            conn6.Open()
+            cmd6 = New MySqlCommand(qry, conn6)
+            If cmd6.ExecuteNonQuery() > 0 Then
+                MessageBox.Show("Item deleted")
+            End If
+            cmd6 = New MySqlCommand(qry1, conn6)
+            cmd6.ExecuteNonQuery()
+            conn6.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            conn6.Close()
+        End Try
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
